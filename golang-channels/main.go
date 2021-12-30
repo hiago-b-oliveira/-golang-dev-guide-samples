c230e5f2d09beb502e872263efb66aadc074dd42
Hiago Borges <hiago.borges@pipefy.com>
2021-12-30 11:47:56 -0300

    sync

---
+++ golang-channels/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+		"https://aaaa.requestcatcher.com/test",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(2 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, err)
+		c <- link
+		return
+	}
+	fmt.Println(link, "healthy")
+	c <- link
+}
